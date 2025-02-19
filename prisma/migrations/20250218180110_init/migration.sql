1f816c1cbfb1f00bcc72bc5690b51a0d2fd03f63
mohabbaticicle <mohabbatali@iciclecorporation.com>
2025-02-19 23:16:05 +0600

    Create A projects with graphql

---
+++ prisma/migrations/20250218180110_init/migration.sql
@@ -0,0 +1,41 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "bio" TEXT,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
---
+++ src/resolvers/index.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const prisma = new PrismaClient();
+
+interface userInfo {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const resolvers = {
+  Query: {
+    users: async (parent: any, args: any, context: any) => {
+      const result = await prisma.user.findMany();
+      return result;
+    },
+  },
+  Mutation: {
+    signup: async (parent: any, args: userInfo, context: any) => {
+      const hashedPassword = await bcrypt.hash(args.password, 12);
+      const newUser = await prisma.user.create({
+        data: {
+          name: args.name,
+          email: args.email,
+          password: hashedPassword,
+        },
+      });
+      const token = jwt.sign({ userId: newUser.id }, "secret", {
+        expiresIn: "7d",
+      });
+      // console.log(token);
+      return { ...newUser, token };
+    },
+  },
+};
